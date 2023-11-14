9b0a5d5528eca8aa23e9410f3d3b6de85be58c31
Abdelrahman ali <121943201+abdelrahman-Ali-moh@users.noreply.github.com>
2023-11-14 02:44:21 +0200

    housing price.sql

--- housing price.sql
+++ housing price.sql
@@ -4,18 +4,18 @@ select * from dbo.['2016$']
 --clean rows for null values
 DELETE FROM dbo.['2016$'] 
 WHERE id IS NULL;
--- value built year when the price of house > 250000
+-- grade and price of house > 250000
 select id,[grade of the house],[Built Year] from dbo.['2016$'] where Price >250000
 -- show house prices when grade of house = 10 star 
 SELECT Price
 FROM dbo.['2016$']
 WHERE [grade of the house]=10;
--- count school near house
+-- count school near house built in year is greater than 1980
 SELECT count([Number of schools nearby]) as "total of school"
 FROM dbo.['2016$'] where [Built Year] >=1980
 -- avg price houses 2016
 select  avg(Price) as "avg price of house"  from dbo.['2016$'] 
--- join table 2016,2017
+-- join table named 2016,2017
 SELECT * INTO joined FROM dbo.['2016$'] UNION ALL SELECT * FROM dbo.['2017$'];
 -- show joined table
 select * from joined
@@ -30,9 +30,9 @@ select  sum(Price)/COUNT(Price) as "mean price of house"  from joined
 select SUM(Price) as "sum of houses" from joined
 -- min price of houses
 select min(Price) as "min of houses" from joined
--- max price of price
+-- max price of houses
 select max(Price) as "max of houses" from joined
--- count price of price
+-- count price of houses
 select count(Price) as "count of houses" from joined
 --As we can see maximum number of houses are with 3 bedrooms
 select *  from joined where [number of bedrooms]>3
@@ -40,7 +40,3 @@ select *  from joined where [number of bedrooms]>3
 select *  from joined where [Distance from the airport]>75
 -- count living area
 select *  from dbo.joined order by [living area] 
-
-
-
-
